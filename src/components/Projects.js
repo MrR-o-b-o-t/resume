e0b6cd4cb5be8d14a27700b1e19d7174fa9361b3
Web-Dev-Jr <samkautz@yahoo.com>
2022-01-31 10:03:37 -0600

    update href for project in Project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
-import "./Projects.css"
-import React from "react"
+import './Projects.css'
+import React from 'react'
 import {
   MDBRow,
   MDBCol,
@@ -8,79 +8,79 @@ import {
   MDBCardImage,
   MDBCardText,
   MDBContainer,
-} from "mdb-react-ui-kit"
+} from 'mdb-react-ui-kit'
 
-import boePortfolio from "../images/boePortfolio.PNG"
-import titusDocConvert from "../images/titusDocConvert.PNG"
-import yips from "../images/yips.PNG"
-import titusCms from "../images/titusCms.PNG"
+import boePortfolio from '../images/boePortfolio.PNG'
+import titusDocConvert from '../images/titusDocConvert.PNG'
+import yips from '../images/yips.PNG'
+import titusCms from '../images/titusCms.PNG'
 
 export default function Projects() {
   return (
     <div
       style={{
-        backgroundColor: "#F7F7F7",
-        marginTop: "100px",
-        paddingBottom: "130px",
+        backgroundColor: '#F7F7F7',
+        marginTop: '100px',
+        paddingBottom: '130px',
       }}
     >
-      <div id="project__container">
-        <div id="project__title">
-          <h1 style={{ fontWeight: "600", fontSize: "48px" }}>PROJECTS</h1>
+      <div id='project__container'>
+        <div id='project__title'>
+          <h1 style={{ fontWeight: '600', fontSize: '48px' }}>PROJECTS</h1>
         </div>
         <MDBContainer>
-          <MDBRow className="row-cols-1 row-cols-md-3 g-4">
-            <MDBCol className="portfolio__cards">
-              <MDBCard className="h-100">
-                <MDBCardImage src={boePortfolio} alt="..." position="top" />
-                <MDBCardBody className="text-center">
-                  <a href="https://boetexas.com/" target="blank">
-                    <button className="project__button">LEARN MORE</button>
+          <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+            <MDBCol className='portfolio__cards'>
+              <MDBCard className='h-100'>
+                <MDBCardImage src={boePortfolio} alt='...' position='top' />
+                <MDBCardBody className='text-center'>
+                  <a href='https://boe-texas.herokuapp.com/' target='blank'>
+                    <button className='project__button'>LEARN MORE</button>
                   </a>
-                  <MDBCardText className="card__title mt-3">
+                  <MDBCardText className='card__title mt-3'>
                     Bank of England Texas
                   </MDBCardText>
-                  <MDBCardText className="mt-3">
+                  <MDBCardText className='mt-3'>
                     React JS, Bootstrap
                   </MDBCardText>
                 </MDBCardBody>
               </MDBCard>
             </MDBCol>
-            <MDBCol className="portfolio__cards">
-              <MDBCard className="h-100">
-                <MDBCardImage src={titusDocConvert} alt="..." position="top" />
-                <MDBCardBody className="text-center">
-                  <button className="project__button">LEARN MORE</button>
-                  <MDBCardText className="card__title mt-3">
+            <MDBCol className='portfolio__cards'>
+              <MDBCard className='h-100'>
+                <MDBCardImage src={titusDocConvert} alt='...' position='top' />
+                <MDBCardBody className='text-center'>
+                  <button className='project__button'>LEARN MORE</button>
+                  <MDBCardText className='card__title mt-3'>
                     Titus Document Convert
                   </MDBCardText>
-                  <MDBCardText className="mt-3">
+                  <MDBCardText className='mt-3'>
                     React JS, Python, Pandas
                   </MDBCardText>
                 </MDBCardBody>
               </MDBCard>
             </MDBCol>
-            <MDBCol className="portfolio__cards">
-              <MDBCard className="h-100">
-                <MDBCardImage src={yips} alt="..." position="top" />
-                <MDBCardBody className="text-center">
-                  <a href="https://yips.co/" target="blank">
-                    <button className="project__button">LEARN MORE</button>
+            <MDBCol className='portfolio__cards'>
+              <MDBCard className='h-100'>
+                <MDBCardImage src={yips} alt='...' position='top' />
+                <MDBCardBody className='text-center'>
+                  <a href='https://yips.co/' target='blank'>
+                    <button className='project__button'>LEARN MORE</button>
                   </a>
-                  <MDBCardText className="card__title mt-3">YIPS</MDBCardText>
-                  <MDBCardText className="mt-3">Liquid, CSS, HTML</MDBCardText>
+                  <MDBCardText className='card__title mt-3'>YIPS</MDBCardText>
+                  <MDBCardText className='mt-3'>Liquid, CSS, HTML</MDBCardText>
                 </MDBCardBody>
               </MDBCard>
             </MDBCol>
-            <MDBCol className="portfolio__cards">
-              <MDBCard className="h-100">
-                <MDBCardImage src={titusCms} alt="..." position="top" />
-                <MDBCardBody className="text-center">
-                  <button className="project__button">LEARN MORE</button>
-                  <MDBCardText className="card__title mt-3">
+            <MDBCol className='portfolio__cards'>
+              <MDBCard className='h-100'>
+                <MDBCardImage src={titusCms} alt='...' position='top' />
+                <MDBCardBody className='text-center'>
+                  <button className='project__button'>LEARN MORE</button>
+                  <MDBCardText className='card__title mt-3'>
                     Titus CMS
                   </MDBCardText>
-                  <MDBCardText className="mt-3">
+                  <MDBCardText className='mt-3'>
                     React JS, MUI, Node.js, MangoDB, SQL
                   </MDBCardText>
                 </MDBCardBody>
