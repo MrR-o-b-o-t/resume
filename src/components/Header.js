0a7ca2a89a25665d6fca0c83ebb617dbc41336a5
Web-Dev-Jr <samkautz@yahoo.com>
2022-03-06 15:37:34 -0600

    Add static background for Header.js in FireFox browser

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,61 @@
-import "./Header.css"
-import React from "react"
-import { MDBIcon } from "mdb-react-ui-kit"
-import stars from "../images/stars.mp4"
+import './Header.css'
+import React from 'react'
+import { MDBIcon } from 'mdb-react-ui-kit'
+import { isFirefox } from 'react-device-detect'
+import stars from '../images/stars.mp4'
+import nebula from '../images/nebula.jpg'
 
 export default function Header() {
-  return (
-    <div
-      className="d-flex justify-content-center align-items-center"
-      id="header__container"
-    >
-      <video
-        loop={true}
-        autoPlay
-        muted
-        controls={false}
-        playsInline
-        id="bg-video"
-        src={stars}
-        type="video/mp4"
-      />
-      <div id="header__inner__div">
-        <h1>
-          Hello, I'm <span>Sam Kautz</span>.
-        </h1>
-        <h1>I'm a full-stack web developer.</h1>
-        <a href="#project__container">
-          <button id="work__btn">
-            View my work
-            <MDBIcon id="work__btn__icon" fas size="xs" icon="arrow-right" />
-          </button>
-        </a>
+  if (!isFirefox) {
+    return (
+      <div
+        className='d-flex justify-content-center align-items-center'
+        id='header__container'
+      >
+        <video
+          loop={true}
+          autoPlay={true}
+          muted
+          controls={false}
+          playsInline
+          id='bg-video'
+          src={stars}
+          type='video/mp4'
+        />
+        <div id='header__inner__div'>
+          <h1>
+            Hello, I'm <span>Sam Kautz</span>.
+          </h1>
+          <h1>I'm a full-stack web developer.</h1>
+          <a href='#project__container'>
+            <button id='work__btn'>
+              View my work
+              <MDBIcon id='work__btn__icon' fas size='xs' icon='arrow-right' />
+            </button>
+          </a>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div
+        className='d-flex justify-content-center align-items-center'
+        id='header__container'
+      >
+        <img src={nebula} alt='Shooting Stars Background' id='bg-video' />
+        <div id='header__inner__div'>
+          <h1>
+            Hello, I'm <span>Sam Kautz</span>.
+          </h1>
+          <h1>I'm a full-stack web developer.</h1>
+          <a href='#project__container'>
+            <button id='work__btn'>
+              View my work
+              <MDBIcon id='work__btn__icon' fas size='xs' icon='arrow-right' />
+            </button>
+          </a>
+        </div>
+      </div>
+    )
+  }
 }
