6a478d799721d9ed51911d812704ec21eb309d30
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2022-12-27 14:57:19 -0600

    Updated index.css with update for x-overflow on mobile devices

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from "./App";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "./context/ThemeContext";
+import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
