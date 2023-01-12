754c19ac58f8c36247a9e9f27b98a882734d6f40
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-01-12 17:10:50 -0600

    update url for devConnector project. Updatge discreption for devConnector.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,8 @@ import {
 } from "mdb-react-ui-kit";
 
 import boePortfolio from "../images/boePortfolio.PNG";
-import titusDocConvert from "../images/titusDocConvert.PNG";
+import dC1 from "../images/dc-1.png";
+import dC2 from "../images/dc-2.png";
 import cherry from "../images/cherry.png";
 import titusCms from "../images/titusCms.PNG";
 import fbl from "../images/fbl.PNG";
@@ -31,6 +32,9 @@ export default function Projects() {
   const [basicModal, setBasicModal] = useState(false);
   const toggleShow = () => setBasicModal(!basicModal);
 
+  const [dcModal, setdcModal] = useState(false);
+  const toggleShow2 = () => setdcModal(!dcModal);
+
   const { mode } = useTheme();
   return (
     <div className={`project__container ${mode}`}>
@@ -40,6 +44,7 @@ export default function Projects() {
         </div>
         <MDBContainer>
           <MDBRow className="row-cols-1 row-cols-md-3 g-4">
+            {/* BOE Card */}
             <MDBCol className="portfolio__cards">
               <MDBCard className="h-100">
                 <MDBCardImage src={boePortfolio} alt="..." position="top" />
@@ -108,31 +113,90 @@ export default function Projects() {
                 </MDBModalDialog>
               </MDBModal>
             </MDBCol>
+
+            {/* Dev Connector Card */}
             <MDBCol className="portfolio__cards">
               <MDBCard className="h-100">
-                <MDBCardImage src={titusDocConvert} alt="..." position="top" />
+                <MDBCardImage
+                  src={dC1}
+                  alt="..."
+                  position="top"
+                  style={{ minHeight: "355px" }}
+                />
                 <MDBCardBody className="text-center">
-                  <a
-                    href="https://titus-doc-converter.herokuapp.com/"
-                    target="blank"
-                  >
-                    <button className="project__button">LEARN MORE</button>
-                  </a>
+                  <button className="project__button" onClick={toggleShow2}>
+                    LEARN MORE
+                  </button>
                   <MDBCardText className="card__title mt-3">
-                    Titus Document Convert
-                  </MDBCardText>
-                  <MDBCardText className="mt-3">
-                    React JS, Python, Pandas
+                    Dev Connector
                   </MDBCardText>
                 </MDBCardBody>
+
+                <MDBModal show={dcModal} setShow={setdcModal} tabIndex="-1">
+                  <MDBModalDialog size="l" centered>
+                    <MDBModalContent>
+                      <MDBModalHeader>
+                        <MDBModalTitle>Dev Connector</MDBModalTitle>
+                        <MDBBtn
+                          className="btn-close"
+                          color="none"
+                          onClick={toggleShow2}
+                        ></MDBBtn>
+                      </MDBModalHeader>
+                      <MDBModalBody>
+                        <MDBCard className="h-100">
+                          <MDBCardImage src={dC2} alt="..." position="top" />
+                          <MDBCardBody className="text-center">
+                            <a
+                              href="https://devtracker-b3494.firebaseapp.com/"
+                              target="blank"
+                            >
+                              <button className="project__button">
+                                View Live
+                              </button>
+                            </a>
+                            <MDBCardText className="mt-3">
+                              Built for myself and fellow devs working on
+                              projects in our free time using React.js, Node.js,
+                              MUIReact, CSS, and a few npm packages for the
+                              frontend. The backend is handled using FireBase.
+                              <br />
+                              <br />I built this application for me and my team
+                              members to keep track of bugs and other tasks. I
+                              took inspiration from a Jira board my team used
+                              during my day job. Users are able to create a
+                              profile and login with FireBase. Data for projects
+                              are also stored with FireBase and can be
+                              dynamically displayed on a users dashboard
+                              depending on that persons user rights. Users can
+                              see which developers are online in real time and
+                              add comments under individual tasks. Task can be
+                              completed and removed but only by the user who
+                              created the task.
+                            </MDBCardText>
+                          </MDBCardBody>
+                        </MDBCard>
+                      </MDBModalBody>
+
+                      <MDBModalFooter>
+                        <MDBBtn color="secondary" onClick={toggleShow2}>
+                          Close
+                        </MDBBtn>
+                      </MDBModalFooter>
+                    </MDBModalContent>
+                  </MDBModalDialog>
+                </MDBModal>
               </MDBCard>
             </MDBCol>
+            {/* Cherry Coatings Card */}
             <MDBCol className="portfolio__cards">
               <MDBCard className="h-100">
                 <MDBCardImage src={cherry} alt="..." position="top" />
                 <MDBCardBody className="text-center">
                   <a href="https://cherrycoatings.com/" target="blank">
-                    <button className="project__button">LEARN MORE</button>
+                    <button className="project__button" onClick={toggleShow}>
+                      LEARN MORE
+                    </button>
                   </a>
                   <MDBCardText className="card__title mt-3">
                     Cherry Coatings
@@ -157,6 +221,7 @@ export default function Projects() {
                 </MDBCardBody>
               </MDBCard>
             </MDBCol>
+            {/* Forbes Barrentine Law Card */}
             <MDBCol className="portfolio__cards">
               <MDBCard className="h-100">
                 <MDBCardImage src={fbl} alt="..." position="top" />
