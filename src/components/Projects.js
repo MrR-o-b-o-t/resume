a397daab5eb755427378ef7caab0d6d77e735050
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-01-13 14:24:14 -0600

    Update card styles in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,8 @@ import {
   MDBBtn,
   MDBCol,
   MDBCard,
+  MDBCardFooter,
+  MDBCardTitle,
   MDBCardBody,
   MDBCardImage,
   MDBCardText,
@@ -47,228 +49,210 @@ export default function Projects() {
         <MDBContainer>
           <MDBRow className="row-cols-1 row-cols-md-3 g-4">
             {/* BOE Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade left>
-                <MDBCard className="h-100">
-                  <MDBCardImage src={boePortfolio} alt="..." position="top" />
-                  <MDBCardBody className="text-center">
-                    <button onClick={toggleShow} className="project__button">
-                      LEARN MORE
-                    </button>
-                    <MDBCardText className="card__title mt-3">
-                      Bank of England Texas
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
+            <Fade left>
+              <MDBCard className="h-100">
+                <MDBCardImage src={boePortfolio} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">
+                    Bank of England Texas
+                  </MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <button onClick={toggleShow} className="project__button">
+                    LEARN MORE
+                  </button>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
 
-              <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
-                <MDBModalDialog size="l" centered>
-                  <MDBModalContent>
-                    <MDBModalHeader>
-                      <MDBModalTitle>Bank of England Texas</MDBModalTitle>
-                      <MDBBtn
-                        className="btn-close"
-                        color="none"
-                        onClick={toggleShow}
-                      ></MDBBtn>
-                    </MDBModalHeader>
-                    <MDBModalBody>
-                      <MDBCard className="h-100">
-                        <MDBCardImage
-                          src={boePortfolio}
-                          alt="..."
-                          position="top"
-                        />
-                        <MDBCardBody className="text-center">
-                          <a
-                            href="https://boe-texas.firebaseapp.com/"
-                            target="blank"
+            <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
+              <MDBModalDialog size="l" centered>
+                <MDBModalContent>
+                  <MDBModalHeader>
+                    <MDBModalTitle>Bank of England Texas</MDBModalTitle>
+                    <MDBBtn
+                      className="btn-close"
+                      color="none"
+                      onClick={toggleShow}
+                    ></MDBBtn>
+                  </MDBModalHeader>
+                  <MDBModalBody>
+                    <MDBCard className="h-100">
+                      <MDBCardImage
+                        src={boePortfolio}
+                        alt="..."
+                        position="top"
+                      />
+                      <MDBCardBody className="text-center">
+                        <a
+                          href="https://boe-texas.firebaseapp.com/"
+                          target="blank"
+                        >
+                          <button
+                            onClick={toggleShow}
+                            className="project__button"
                           >
-                            <button
-                              onClick={toggleShow}
-                              className="project__button"
-                            >
-                              View Live
-                            </button>
-                          </a>
-                          <MDBCardText className="mt-3">
-                            Built for Bank of England Texas using React JS,
-                            Node.js, MDBReact, JavaScript, GSAP, and CSS.
-                            <br />
-                            <br />
-                            This was my first industry level project. As a
-                            junior developer and working solo it presented many
-                            challenges and many learning oppurtunites. It
-                            provided me the oppurtunity to take a deep dive into
-                            React, Redux, Node, MDBReact, and Git for version
-                            control.
-                          </MDBCardText>
-                        </MDBCardBody>
-                      </MDBCard>
-                    </MDBModalBody>
+                            View Live
+                          </button>
+                        </a>
+                        <MDBCardText className="mt-3">
+                          Built for Bank of England Texas using React JS,
+                          Node.js, MDBReact, JavaScript, GSAP, and CSS.
+                          <br />
+                          <br />
+                          This was my first industry level project. As a junior
+                          developer and working solo it presented many
+                          challenges and many learning oppurtunites. It provided
+                          me the oppurtunity to take a deep dive into React,
+                          Redux, Node, MDBReact, and Git for version control.
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBModalBody>
 
-                    <MDBModalFooter>
-                      <MDBBtn color="secondary" onClick={toggleShow}>
-                        Close
-                      </MDBBtn>
-                    </MDBModalFooter>
-                  </MDBModalContent>
-                </MDBModalDialog>
-              </MDBModal>
-            </MDBCol>
+                  <MDBModalFooter>
+                    <MDBBtn color="secondary" onClick={toggleShow}>
+                      Close
+                    </MDBBtn>
+                  </MDBModalFooter>
+                </MDBModalContent>
+              </MDBModalDialog>
+            </MDBModal>
 
             {/* Dev Connector Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade bottom>
-                <MDBCard className="h-100">
-                  <MDBCardImage
-                    src={dC1}
-                    alt="..."
-                    position="top"
-                    style={{ minHeight: "355px" }}
-                  />
-                  <MDBCardBody className="text-center">
-                    <button className="project__button" onClick={toggleShow2}>
-                      LEARN MORE
-                    </button>
-                    <MDBCardText className="card__title mt-3">
-                      Dev Connector
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
+            <Fade bottom>
+              <MDBCard className="h-100">
+                <MDBCardImage src={dC1} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">
+                    Dev Connector
+                  </MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <button className="project__button" onClick={toggleShow2}>
+                    LEARN MORE
+                  </button>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
 
-              <MDBModal show={dcModal} setShow={setdcModal} tabIndex="-1">
-                <MDBModalDialog size="l" centered>
-                  <MDBModalContent>
-                    <MDBModalHeader>
-                      <MDBModalTitle>Dev Connector</MDBModalTitle>
-                      <MDBBtn
-                        className="btn-close"
-                        color="none"
-                        onClick={toggleShow2}
-                      ></MDBBtn>
-                    </MDBModalHeader>
-                    <MDBModalBody>
-                      <MDBCard className="h-100">
-                        <MDBCardImage src={dC2} alt="..." position="top" />
-                        <MDBCardBody className="text-center">
-                          <a
-                            href="https://devtracker-b3494.firebaseapp.com/"
-                            target="blank"
-                          >
-                            <button className="project__button">
-                              View Live
-                            </button>
-                          </a>
-                          <MDBCardText className="mt-3">
-                            Built for myself and fellow devs working on projects
-                            in our free time using React.js, Node.js, MUIReact,
-                            CSS, and a few npm packages for the frontend. The
-                            backend is handled using FireBase.
-                            <br />
-                            <br />I built this application for me and my team
-                            members to keep track of bugs and other tasks. I
-                            took inspiration from a Jira board my team used
-                            during my day job. Users are able to create a
-                            profile and login with FireBase. Data for projects
-                            are also stored with FireBase and can be dynamically
-                            displayed on a users dashboard depending on that
-                            persons user rights. Users can see which developers
-                            are online in real time and add comments under
-                            individual tasks. Task can be completed and removed
-                            but only by the user who created the task.
-                          </MDBCardText>
-                        </MDBCardBody>
-                      </MDBCard>
-                    </MDBModalBody>
+            <MDBModal show={dcModal} setShow={setdcModal} tabIndex="-1">
+              <MDBModalDialog size="l" centered>
+                <MDBModalContent>
+                  <MDBModalHeader>
+                    <MDBModalTitle>Dev Connector</MDBModalTitle>
+                    <MDBBtn
+                      className="btn-close"
+                      color="none"
+                      onClick={toggleShow2}
+                    ></MDBBtn>
+                  </MDBModalHeader>
+                  <MDBModalBody>
+                    <MDBCard className="h-100">
+                      <MDBCardImage src={dC2} alt="..." position="top" />
+                      <MDBCardBody className="text-center">
+                        <a
+                          href="https://devtracker-b3494.firebaseapp.com/"
+                          target="blank"
+                        >
+                          <button className="project__button">View Live</button>
+                        </a>
+                        <MDBCardText className="mt-3">
+                          Built for myself and fellow devs working on projects
+                          in our free time using React.js, Node.js, MUIReact,
+                          CSS, and a few npm packages for the frontend. The
+                          backend is handled using FireBase.
+                          <br />
+                          <br />I built this application for me and my team
+                          members to keep track of bugs and other tasks. I took
+                          inspiration from a Jira board my team used during my
+                          day job. Users are able to create a profile and login
+                          with FireBase. Data for projects are also stored with
+                          FireBase and can be dynamically displayed on a users
+                          dashboard depending on that persons user rights. Users
+                          can see which developers are online in real time and
+                          add comments under individual tasks. Task can be
+                          completed and removed but only by the user who created
+                          the task.
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBModalBody>
 
-                    <MDBModalFooter>
-                      <MDBBtn color="secondary" onClick={toggleShow2}>
-                        Close
-                      </MDBBtn>
-                    </MDBModalFooter>
-                  </MDBModalContent>
-                </MDBModalDialog>
-              </MDBModal>
-            </MDBCol>
+                  <MDBModalFooter>
+                    <MDBBtn color="secondary" onClick={toggleShow2}>
+                      Close
+                    </MDBBtn>
+                  </MDBModalFooter>
+                </MDBModalContent>
+              </MDBModalDialog>
+            </MDBModal>
+
             {/* Cherry Coatings Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade right>
-                <MDBCard className="h-100">
-                  <MDBCardImage src={cherry} alt="..." position="top" />
-                  <MDBCardBody className="text-center">
-                    <a href="https://cherrycoatings.com/" target="blank">
-                      <button className="project__button" onClick={toggleShow}>
-                        LEARN MORE
-                      </button>
-                    </a>
-                    <MDBCardText className="card__title mt-3">
-                      Cherry Coatings
-                    </MDBCardText>
-                    <MDBCardText className="mt-3">
-                      Liquid, CSS, HTML, JS
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
-            </MDBCol>
-            {/* Titus CMS Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade left>
-                <MDBCard className="h-100">
-                  <MDBCardImage src={titusCms} alt="..." position="top" />
-                  <MDBCardBody className="text-center">
+            <Fade right>
+              <MDBCard className="h-100">
+                <MDBCardImage src={cherry} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">
+                    Cherry Coatings
+                  </MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <a href="https://cherrycoatings.com/" target="blank">
                     <button className="project__button">LEARN MORE</button>
-                    <MDBCardText className="card__title mt-3">
-                      Titus CMS
-                    </MDBCardText>
-                    <MDBCardText className="mt-3">
-                      React JS, MUI, Redux, Node.js, MangoDB
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
-            </MDBCol>
+                  </a>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
+
+            {/* Titus CMS Card */}
+            <Fade left>
+              <MDBCard className="h-100">
+                <MDBCardImage src={titusCms} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">Titus CMS</MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <a href="/" target="blank">
+                    <button className="project__button">LEARN MORE</button>
+                  </a>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
+
             {/* Forbes Barrentine Law Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade bottom>
-                <MDBCard className="h-100">
-                  <MDBCardImage src={fbl} alt="..." position="top" />
-                  <MDBCardBody className="text-center">
-                    <a href="https://forbesbarrentinelaw.com/" target="blank">
-                      <button className="project__button">LEARN MORE</button>
-                    </a>
-                    <MDBCardText className="card__title mt-3">
-                      Forbes Barrentine Law
-                    </MDBCardText>
-                    <MDBCardText className="mt-3">
-                      Wordpress, PHP, CSS
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
-            </MDBCol>
+            <Fade bottom>
+              <MDBCard className="h-100">
+                <MDBCardImage src={fbl} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">
+                    Forbes Barrentine Law
+                  </MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <a href="https://forbesbarrentinelaw.com/" target="blank">
+                    <button className="project__button">LEARN MORE</button>
+                  </a>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
+
             {/* JS Card */}
-            <MDBCol className="portfolio__cards">
-              <Fade right>
-                <MDBCard className="h-100">
-                  <MDBCardImage src={jas} alt="..." position="top" />
-                  <MDBCardBody className="text-center">
-                    <a href="https://johnsonandsons.net/" target="blank">
-                      <button className="project__button">LEARN MORE</button>
-                    </a>
-                    <MDBCardText className="card__title mt-3">
-                      Johnson and Sons Commercial Floors
-                    </MDBCardText>
-                    <MDBCardText className="mt-3">
-                      Wordpress, PHP, CSS
-                    </MDBCardText>
-                  </MDBCardBody>
-                </MDBCard>
-              </Fade>
-            </MDBCol>
+            <Fade right>
+              <MDBCard className="h-100">
+                <MDBCardImage src={jas} alt="..." position="top" />
+                <MDBCardBody>
+                  <MDBCardTitle className="text-center">
+                    Johnson and Sons Commercial Floors
+                  </MDBCardTitle>
+                </MDBCardBody>
+                <MDBCardFooter className="text-center">
+                  <a href="https://johnsonandsons.net/" target="blank">
+                    <button className="project__button">LEARN MORE</button>
+                  </a>
+                </MDBCardFooter>
+              </MDBCard>
+            </Fade>
           </MDBRow>
         </MDBContainer>
       </div>
